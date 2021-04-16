e063447c7481ccd20f09cf71e640d8b60678d23b
William Moon <william-moon@lambdastudents.com>
2021-04-16 12:18:48 -0700

    finished character names

--- src/App.js
+++ src/App.js
@@ -1,31 +1,37 @@
 import React, {useState, useEffect} from 'react';
 import axios from "axios";
 import './App.css';
-import CharacterList from "./components/Character"
+import Character, {CharacterInfo} from "./components/Character"
+
 
 const App = () => {
-  const [info, setInfo] = useState({})
+  const [info, setInfo] = useState([])
+  const [sharedInfo, setSharedInfo] = useState(true)
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
   useEffect(() => {
       axios
-      .get("https://swapi.dev/api/people")
+      .get("https://swapi.dev/api/people/")
       .then(res => {
+        console.log(res.data)
         setInfo(res.data)
-      }, [])
+      })
       .catch(err => {
         console.log(err)
       })
-  })
+  }, [])
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
-  
+  // function showSharedInfo(){
+  //   setSharedInfo()
+  // }
+    
 
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
-      <CharacterList list={info} />
+      <Character character={info} key />
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -2,16 +2,27 @@
 import React, {useState, useEffect} from "react";
 import styled from "styled-components";
 
-export default function CharacterList({ list }){
-const [characterList, setCharacterList] = useState({})
+export default function Character(props) {
+    const {character} = props
+    return (
+        <div>
+           {character.map((item) => {
+               return (
+                   <CharacterInfo 
+                   key={item.url}
+                   characterObj={item}
+                   />
+               )
+           })}
+        </div>
+    )
+}
 
-
-console.log({list})
-debugger
-
-return (
-    <div>
-        hello
-    </div>
-)
+const CharacterInfo = (props) => {
+    const {characterObj} = props
+    return (
+        <>
+        <h1>{characterObj.name}</h1>
+        </>
+    )
 }
