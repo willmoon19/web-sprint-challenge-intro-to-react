5bdc8bf704c38e0ec8fb93cc3a359971aba9c97b
William Moon <william-moon@lambdastudents.com>
2021-04-16 09:07:29 -0700

    got api then drilled to character comp

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from "axios";
 import './App.css';
+import Characters from "./components/Character"
 
 const App = () => {
+  const [info, setInfo] = useState({})
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-
+  useEffect(() => {
+      axios
+      .get("https://swapi.dev/api/people")
+      .then((res) => {
+        setInfo(res.data)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  })
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  
 
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      <CharacterList list={info} />
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,8 @@
 // Write your Character component here
+import React, {useState, useEffect} from "react";
+import styled from "styled-components";
+
+const CharacterList = ({ list }) => {
+const [characterList, setCharacterList] = useState({})
+
+}
